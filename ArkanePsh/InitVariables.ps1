295870050e2e727681db3e753fa594c3a42f817a
cerebrate <avatar@arkane-systems.net>
2015-07-26 06:12:41 -0500

    Types; variable fixes.

--- ArkanePsh/InitVariables.ps1
+++ ArkanePsh/InitVariables.ps1
@@ -11,14 +11,14 @@ function Set-ArkaneVariable ($name, $value)
 
 # Set up relevant variables
 
-Set-ProfileVariable myDocuments          ([System.Environment]::GetFolderPath(5))
+Set-ArkaneVariable myDocuments          ([System.Environment]::GetFolderPath(5))
 
-Set-ProfileVariable executableExtensions (".exe",".ps1",".cmd",".py")
-Set-ProfileVariable archiveExtensions    (".7z",".zip",".gz",".bz2",".rar",".tar",".z")
+Set-ArkaneVariable executableExtensions (".exe",".ps1",".cmd",".py")
+Set-ArkaneVariable archiveExtensions    (".7z",".zip",".gz",".bz2",".rar",".tar",".z")
 
-Set-ProfileVariable windowsIdentity      ([System.Security.Principal.WindowsIdentity]::GetCurrent())
-Set-ProfileVariable windowsPrincipal     (new-object System.Security.Principal.WindowsPrincipal $windowsIdentity)
-Set-ProfileVariable isAdmin              ($windowsPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator))
+Set-ArkaneVariable windowsIdentity      ([System.Security.Principal.WindowsIdentity]::GetCurrent())
+Set-ArkaneVariable windowsPrincipal     (new-object System.Security.Principal.WindowsPrincipal $windowsIdentity)
+Set-ArkaneVariable isAdmin              ($windowsPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator))
 
 # Delete internal helper functions
 
