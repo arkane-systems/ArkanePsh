8d127cd257a68ebb856d966f495d552e9160ec20
Alistair Young <avatar@arkane-systems.net>
2019-06-22 13:24:13 -0500

    Enable dotnet-suggest automatically.

---
+++ ArkanePsh/Scripts/EnableDotnetSuggest.ps1
@@ -0,0 +1,19 @@
+﻿# enable 'dotnet suggest' if it is available
+
+if (Get-Command "dotnet-suggest" -ErrorAction SilentlyContinue) 
+{
+	$availableToComplete = (dotnet-suggest list) | Out-String
+	$availableToCompleteArray = $availableToComplete.Split([Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries) 
+
+
+	Register-ArgumentCompleter -Native -CommandName $availableToCompleteArray -ScriptBlock {
+		param($commandName, $wordToComplete, $cursorPosition)
+		$fullpath = (Get-Command $wordToComplete.CommandElements[0]).Source
+
+		$arguments = $wordToComplete.Extent.ToString().Replace('"', '\"')
+		dotnet-suggest get -e $fullpath --position $cursorPosition -- "$arguments" | ForEach-Object {
+			[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+		}
+	}
+	$env:DOTNET_SUGGEST_SCRIPT_VERSION = "1.0.0"
+}
